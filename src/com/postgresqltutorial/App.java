9f84fff4da0775fdef5ee087cb0f2454c0df6bb7
Sharmarke Duale <sduale01@gmail.com>
2019-04-21 13:23:15 -0500

    rearrange db connection files

--- src/com/postgresqltutorial/App.java
+++ src/com/postgresqltutorial/App.java
@@ -4,26 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class App {
-	private final String url = "jdbc:postgresql://localhost:5432/dvdrental";
-	private final String user = "nopcallowed";
-	private final String password = "";
-	
-	public Connection connect() {
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			System.out.println("Connected to database!");	
-		} catch (SQLException e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		}
-		
-		return conn;
-	}
-	
+public class App {	
 	public static void main(String[] args) {
-		App app = new App();
-		app.connect();
+		DbAdapter dbAdapter = new DbAdapter();
+		dbAdapter.connect();
+//		dbAdapter.disconnect();
 	}
 }
---
+++ src/com/postgresqltutorial/DbAdapter.java
@@ -0,0 +1,54 @@
+package com.postgresqltutorial;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbAdapter {
+	private final String url = "jdbc:postgresql://localhost:5432/dvdrental";
+	private final String user = "nopcallowed";
+	private final String password = "";
+	
+//	Database variables
+	Connection conn = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	
+//	Connect to database
+	public Connection connect() {
+//		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("Connected to database!");	
+		} catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+		
+		return conn;
+	}
+	
+//	Disconnect from database
+	public void disconnect() {
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+			
+			if (rs != null) {
+				rs.close();
+			}
+			
+			if (conn != null) {
+				conn.close();
+			}
+
+			System.out.println("Disconnected from database!");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
